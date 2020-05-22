5e501ed57ddd26ae1a4642098a46d222400d5f65
Roman Svidunovich <raman.svidunovich@netcracker.com>
2020-05-23 00:36:07 +0300

    remove template and fist step

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const http = require('http');
+const express = require('express');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+app.set('port', 3001);
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+http.createServer(app).listen(app.get('port'), () => {
+    console.log(`Express server on port ${app.get('port')}`);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((req, res, next) => {
+    if (req.url === '/') {
+        res.end('<h1>Hello</h1>');
+    } else {
+        next();
+    }
 });
 
-module.exports = app;
+app.use((req, res, next) => {
+  if (req.url === '/test') {
+    throw new Error('Test error');
+  } else {
+    next();
+  }
+});
+
+app.use( (req, res) => {
+  res.send(404, '<h1>Error 404 not found</h1>')
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.send('error');
+});
